955e49d81e2b4d262b7cab604fafcb7d0f9fff8c
royzhu0830 <royzhu456@hotmail.com>
2024-09-12 15:32:29 -0400

    Add background color

--- constants.py
+++ constants.py
@@ -1,2 +1,5 @@
 WIDTH=600
 HEIGHT=600
+
+#colors
+BG_COLOR = (28, 170, 156)
--- tictactoe.py
+++ tictactoe.py
@@ -5,3 +5,17 @@ from constants import *
 
 pygame.init()
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption('TIC TAC TOE AI')
+screen.fill(BG_COLOR)
+
+def main():
+    while True:
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+
+        pygame.display.update()
+
+main()
