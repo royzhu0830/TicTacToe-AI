0be20563cafef2e9fda04d305e9c9049e4674329
royzhu0830 <royzhu456@hotmail.com>
2024-09-17 21:41:35 -0400

    implemented console board
    Yet to draw an actual board but virtual board is working

--- tictactoe.py
+++ tictactoe.py
@@ -1,5 +1,6 @@
 import sys
 import pygame
+import numpy as np
 
 from constants import *
 
@@ -8,10 +9,24 @@ screen = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption('TIC TAC TOE AI')
 screen.fill(BG_COLOR)
 
+class Board:
+
+    def __init__(self):
+        self.squares = np.zeros((ROWS, COLS))
+        
+
+    def marked(self, row, col, player):
+        self.squares[row][col] = player
+
+    def empty(self, row, col):
+        return self.squares[row][col] == 0
 class Game:
     
     def __init__(self):
+        self.consoleBoard = Board()
+        self.player = 1
         self.lines()
+        
 
     def lines(self):
         #vertical lines
@@ -23,11 +38,18 @@ class Game:
         pygame.draw.line(screen, LINE_COLOR, (0, SQSIZE), (WIDTH, SQSIZE), LINE_WIDTH)
         pygame.draw.line(screen, LINE_COLOR, (0, HEIGHT-SQSIZE), (WIDTH, HEIGHT-SQSIZE), LINE_WIDTH)
 
+    def switch(self):
+        if self.player == 1:
+            self.player = 2
+        else:
+            self.player = 1
 
 def main():
 
     #call game objecct
     game = Game()
+    board = game.consoleBoard
+    #mainloop
     while True:
 
         for event in pygame.event.get():
@@ -35,6 +57,21 @@ def main():
                 pygame.quit()
                 sys.exit()
 
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                cur = event.pos
+                #makes row, col more logical numbers
+                row = cur[1] // SQSIZE
+                col = cur[0] // SQSIZE
+                
+                #means can't mark if already marked
+                if board.empty(row, col):
+                    board.marked(row, col, game.player)
+                    
+                    game.switch()
+                    print(board.squares)
+                
+
+                
         pygame.display.update()
 
 main()
