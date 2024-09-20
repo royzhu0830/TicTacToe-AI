d8b0a0373af57dfa4f4be12423af1a0551930ca0
royzhu0830 <royzhu456@hotmail.com>
2024-09-19 21:37:15 -0400

    Implemented functions involving win conditions
    Not using these functions yet until I've implemented the AI

--- tictactoe.py
+++ tictactoe.py
@@ -13,17 +13,64 @@ class Board:
 
     def __init__(self):
         self.squares = np.zeros((ROWS, COLS))
+        self.empty_square = self.squares
+        self.marked_square = 0
+
+    def final(self):
+
+    #return 0 if there is no win yet
+    #returns 1 if player 1 wins
+    #returns 2 if player 2 wins
+
+        #vertical winning
+        for col in range(COLS):
+            if self.squares[0][col] == self.squares[1][col] == self.squares[2][col] != 0:
+                return self.squares[0][col]
         
+        #row winning
+        for row in range(ROWS):
+            if self.squares[row][0] == self.squares[row][1] == self.squares[row][2] != 0:
+                return self.squares[row][0]
+
+        #descending diagonal
+        if self.squares[0][0] == self.squares[1][1] == self.squares[2][2] != 0:
+            return self.squares[0][0]
+        
+        #ascending diagonal
+        if self.squares[2][0] == self.squares[1][1] == self.squares[0][2] != 0:
+            return self.squares[2][0]
+
+        return 0
+        
+    def get_empty_square(self):
+        empty_square = []
+        for row in range(ROWS):
+            for col in range(COLS):
+                if self.empty_square(row, col):
+                    empty_square.append((row, col))
+
+        return empty_square
 
     def marked(self, row, col, player):
         self.squares[row][col] = player
+        self.marked_square = self.marked_square + 1
 
     def empty(self, row, col):
         return self.squares[row][col] == 0
+
+    def full(self):
+        return self.marked_square == 9
+
+    def isempty(self):
+        return self.marked_square == 0
+    
 class Game:
     
     def __init__(self):
         self.consoleBoard = Board()
+        #self.ai = AI()
+        self.gamemode = 'pvp'
+        self.running = True
         self.player = 1
         self.lines()
         
